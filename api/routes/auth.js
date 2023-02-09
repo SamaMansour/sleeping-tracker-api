793c229a4382e5a97769e97dfd51c72e725a23bc
SamaMansour <sma302000@gmail.com>
2023-02-09 11:54:28 +0300

    Add signup:

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,26 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+
 
 router.post("/register", async (req, res) => {
-	
-	if (error) {
-		return res.status(400).send(error.details[0].message);
-	}
-
-	const isUserExist = await User.findOne({ email: req.body.email });
-	if (isUserExist) {
-		return res.status(400).send("user exist");
-	}
-
-	const salt = await bcrypt.genSalt(10);
-	const hashPassword = await bcrypt.hash(req.body.password, salt);
 	const newUser = {
 		username: req.body.username,
 		email: req.body.email,
-		password: hashPassword,
+		password: req.body.password,
 	};
 
 	const user = new User(newUser);
@@ -34,4 +21,4 @@ router.post("/register", async (req, res) => {
   });
 
 
-	module.exports = router;
+module.exports = router;
--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require("express");
+var cors = require('cors')
 const PORT = 1337;
 const app = express();
-require("./db/connection");
+const connectDB =require("./db/connection");
 const authRoute = require("./api/routes/auth");
 const path = require("path");
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+connectDB();
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Header", "*");
-
-  next();
+app.use(function(req, res, next) {
+   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+      next();
 });
 
-app.use("/api/v1/user", authRoute);
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+app.use("/", authRoute);
 
 app.listen(PORT);
