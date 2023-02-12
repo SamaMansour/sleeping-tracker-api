583ad49d4ea53c72717656de73fd135509655583
SamaMansour <sma302000@gmail.com>
2023-02-12 11:45:23 +0300

    Fetch all entries and add nodemon

--- api/routes/entry.js
+++ api/routes/entry.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const entryService = require("../../services/EntryService");
 
 
+router.get("/entries", async (req, res) => {
+  try {
+    const entry = await entryService.getAllEntries();
+    res.json({ data: entry, status: "success" });
+  } catch (err) {
+    res.status(500);
+  }
+});
+
 router.post("/new", async (req, res) => {
   try {
     const entry = await entryService.createEntry(req.body);
