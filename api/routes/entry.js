77f2927e38595667b17fc8e030655066f65b2353
SamaMansour <sma302000@gmail.com>
2023-02-12 12:15:21 +0300

    Add get entry by id

--- api/routes/entry.js
+++ api/routes/entry.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const entryService = require("../../services/EntryService");
 
+router.get("/entries/:id", async (req, res) => {
+  try {
+    const entry = await entryService.getEntryById(req.params.id);
+    res.json({ data: entry, status: "success" });
+  } catch (err) {
+    res.status(500);
+  }
+});
 
 router.get("/entries", async (req, res) => {
   try {
