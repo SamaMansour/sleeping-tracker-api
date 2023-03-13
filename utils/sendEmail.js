c84f4a774d8d843b0aff09f7c88812a4c3734a0b
SamaMansour <sma302000@gmail.com>
2023-03-13 10:54:08 +0300

    Add send email util

---
+++ utils/sendEmail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async(email, subject, text) =>{
+  try {
+    // This should be added to .env
+    const transporter = nodemailer.createTransporter({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, 
+      auth: {
+        user: testAccount.user, 
+        pass: testAccount.pass, 
+    
+    });
+
+   await transporter.sendMail({
+      from: process.env.USER,
+      to: email,
+      subject: subject,
+      text: text,
+    });
+
+    console.log("email sent sucessfully");
+  } catch (error) {
+      console.log(error, "email not sent");
+  }};
+
+module.exports = sendEmail;
+
+
+
